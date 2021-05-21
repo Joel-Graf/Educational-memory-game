322459c430856bf3cd50363893c4261e425c2ad7
Joel Marcos Graf <55204995+Joel-Graf@users.noreply.github.com>
2021-05-20 21:35:26 -0300

    Fazendo lógica de iniciar partida, cronometro e numero de cartas

--- Front/src/Views/ViewGame.vue
+++ Front/src/Views/ViewGame.vue
@@ -1,13 +1,33 @@
 <template>
   <div>
-    <AppGameHeader :pDificulty="dificulty" :pLevel='level'/>
+    <v-overlay :value="!isGameStarted" absolute="true">
+      <!-- TODO: Adicionar Loading ao botão enquanto jogo não carrega -->
+      <v-btn
+        v-if="!isGameStarted"
+        @click="isGameStarted = !isGameStarted"
+        color="primary"
+        x-large
+      >
+        Iniciar Jogo
+      </v-btn>
+    </v-overlay>
+    <AppGameHeader
+      :Dificulty="dificulty"
+      :Level="level"
+      :TimerEnabled="isGameStarted"
+      :CardPairsRemaining="cards.length"
+      :InitialCards="dificulty.cardQuantity"
+    />
     <v-container fluid>
-      <v-row align='center' justify='space-around'>
-        <AppGameCard 
-          v-for="(card, index) in cards" :key="card.id"
-          :Card="card" :Index="index" :CardsFliped="cardsFliped" 
+      <v-row align="center" justify="space-around">
+        <AppGameCard
+          v-for="(card, index) in cards"
+          :key="card.id"
+          :Card="card"
+          :Index="index"
+          :CardsFliped="cardsFliped"
           :IsGameLocked="isGameLocked"
-          @flip-card="handleFlipCard" 
+          @flip-card="handleFlipCard"
           ref="CardsComponents"
         />
       </v-row>
@@ -16,8 +36,8 @@
 </template>
 
 <script>
-import AppGameHeader from '../components/AppGameHeader.vue';
-import AppGameCard from '../components/AppGameCard.vue';
+import AppGameHeader from "../components/AppGameHeader.vue";
+import AppGameCard from "../components/AppGameCard.vue";
 
 export default {
   components: {
@@ -27,70 +47,104 @@ export default {
 
   data() {
     return {
-      level: 'Floresta',
-      dificulty: {id: 1, name: 'Fácil', cardQuantity: 6, timeLimit: 120},
+      level: "Floresta",
+      dificulty: { id: 1, name: "Fácil", cardQuantity: 8, timeLimit: 120 },
       cards: [
-        {id: 1, animalId: 1, name: 'gato', image: 'gato-frente.jpg', sound: 'gato-som.mp3'},
-        {id: 2, animalId: 2, name: 'gato2', image: 'gato-frente.jpg', sound: 'gato-som.mp3'},
-        {id: 3, animalId: 2, name: 'gato3', image: 'gato-frente.jpg', sound: 'gato-som.mp3'},
-        {id: 4, animalId: 1, name: 'gato', image: 'gato-frente.jpg', sound: 'gato-som.mp3'},
+        {
+          id: 1,
+          animalId: 1,
+          name: "gato",
+          image: "gato-frente.jpg",
+          sound: "gato-som.mp3",
+        },
+        {
+          id: 2,
+          animalId: 2,
+          name: "gato2",
+          image: "gato-frente.jpg",
+          sound: "gato-som.mp3",
+        },
+        {
+          id: 3,
+          animalId: 2,
+          name: "gato3",
+          image: "gato-frente.jpg",
+          sound: "gato-som.mp3",
+        },
+        {
+          id: 4,
+          animalId: 1,
+          name: "gato",
+          image: "gato-frente.jpg",
+          sound: "gato-som.mp3",
+        },
       ],
       isGameLocked: false,
-      cardsFliped: []
-    }
+      cardsFliped: [],
+      isGameStarted: false,
+    };
+  },
+  computed: {
+    cardPairsRemaining() {
+      return this.cards.length / 2;
+    },
+  },
+  watch: {
+    cardPairsRemaining(newCardsRemaning) {
+      if (newCardsRemaning == 0) {
+        //TODO: Condição de vitória
+        alert("VENCEU!!");
+      }
+    },
   },
   methods: {
-
     handleFlipCard(cardObject) {
       //Verifica se o jogo está trancado (Existe duas cartas viradas)
-      if(!this.isGameLocked) {
-
+      if (!this.isGameLocked) {
         // Adiciona a lista de cartas viradas
-        this.cardsFliped.push(cardObject)
+        this.cardsFliped.push(cardObject);
 
         // Trava o jogo quando se tem duas cartas viradas
         if (this.cardsFliped.length == 2) {
-          this.isGameLocked = true
+          this.isGameLocked = true;
 
           // Verifica se fechou o par ou nãos
-          let vueInst = this
-          setTimeout(function(){ 
-            var card1 = vueInst.cardsFliped[0]
-            var card2 = vueInst.cardsFliped[1]
-            if(card1.animalId == card2.animalId) {
+          let vueInst = this;
+          setTimeout(function() {
+            var card1 = vueInst.cardsFliped[0];
+            var card2 = vueInst.cardsFliped[1];
+            if (card1.animalId == card2.animalId) {
               // Se fechou par, deleta as cartas
-              vueInst.deleteCards(card1, card2)
+              vueInst.deleteCards(card1, card2);
             } else {
               // Se não fechou, desvira as cartas
-              vueInst.$refs.CardsComponents[card1.index].isFliped = false
-              vueInst.$refs.CardsComponents[card2.index].isFliped = false
+              vueInst.$refs.CardsComponents[card1.index].isFliped = false;
+              vueInst.$refs.CardsComponents[card2.index].isFliped = false;
             }
 
             // Limpa a lista de cartas viradas e destrava o jogo
-            vueInst.cardsFliped = []
-            vueInst.isGameLocked = false
+            vueInst.cardsFliped = [];
+            vueInst.isGameLocked = false;
           }, 1500);
         }
       }
-    }, 
+    },
     deleteCards(card1, card2) {
       // Workaround pra poder deletar via indice
-      if(card1.index > card2.index) {
-        this.$delete(this.cards, card1.index)
-        this.$delete(this.cards, card2.index)
+      if (card1.index > card2.index) {
+        this.$delete(this.cards, card1.index);
+        this.$delete(this.cards, card2.index);
       } else {
-        this.$delete(this.cards, card2.index)
-        this.$delete(this.cards, card1.index)
+        this.$delete(this.cards, card2.index);
+        this.$delete(this.cards, card1.index);
       }
-    }
+    },
   },
   mounted() {
     //TODO: Mostrar tela de loading até montar tudo, depois mostrar o botão
     //TODO: Mostrar Botão de inicio da partida
-  }
+  },
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
--- Front/src/components/AppGameHeader.vue
+++ Front/src/components/AppGameHeader.vue
@@ -6,84 +6,91 @@
 	Progresso: <v-progress-circular :value="20"></v-progress-circular>
 -->
 <template>
-	<v-banner class="mx-5 my-1 px-5">
-		<div class="d-flex justify-space-between">
-			<div>
-				<v-icon
-					color="blue-grey darken-2"
-					size="36"
-				>
-					mdi-cards
-				</v-icon>
-				{{cardPairsRemaining}}/{{dificulty.cardQuantity/2}}
-			</div>
+  <v-banner class="mx-5 my-1 px-5">
+    <div class="d-flex justify-space-between">
+      <div>
+        <v-icon color="blue-grey darken-2" size="36">
+          mdi-cards
+        </v-icon>
+        {{ CardPairsRemaining }}/{{ InitialCards / 2 }}
+      </div>
 
-			<!-- TODO: Colocar o habitat -->
-			<span class="text-h4 font-weight-medium">{{level}} - {{dificulty.name}}</span>
+      <!-- TODO: Colocar o habitat -->
+      <span class="text-h4 font-weight-medium"
+        >{{ Level }} - {{ Dificulty.name }}</span
+      >
 
-			<div>
-				{{timeRemaining | secondsFormatter}}
-				<v-icon
-					color="blue-grey darken-2"
-					size="36"
-				>
-					mdi-clock-time-five-outline
-				</v-icon>
-			</div>
-		</div>
-	</v-banner>
+      <div>
+        {{ timeRemaining | secondsFormatter }}
+        <v-icon color="blue-grey darken-2" size="36">
+          mdi-clock-time-five-outline
+        </v-icon>
+      </div>
+    </div>
+  </v-banner>
 </template>
 
 <script>
 export default {
   props: {
-		pLevel: {
-			type: String,
-			required: true
-		},
-		pDificulty: {
+    Level: {
+      type: String,
+      required: true,
+    },
+    Dificulty: {
       type: Object,
-      required: true
-    }
+      required: true,
+    },
+    CardPairsRemaining: {
+      type: Number,
+      required: true,
+    },
+    InitialCards: {
+      type: Number,
+      required: true,
+    },
+    TimerEnabled: {
+      type: Boolean,
+      required: true,
+    },
   },
   data() {
     return {
-			level: this.pLevel, 
-      dificulty: this.pDificulty, 
-      cardPairsRemaining: this.pDificulty.cardQuantity/2, 
-			timerEnabled: true,
-      timeRemaining: this.pDificulty.timeLimit, 
-    }
+      timeRemaining: this.Dificulty.timeLimit,
+    };
   },
-	watch: {
-		timerEnabled(value) {
-			if (value) {
-				setTimeout(() => {
-					this.timeRemaining--;
-				}, 1000);
-			}
-		},
-		timeRemaining: {
-			handler(value) {
-				if (value > 0 && this.timerEnabled) {
-					setTimeout(() => {
-						this.timeRemaining--;
-					}, 1000);
-				}
-			}, immediate: true
-		}
-	},
-	filters: {
-		secondsFormatter(timeInSeconds) {
-			function str_pad_left(string,pad,length) {return (new Array(length+1).join(pad)+string).slice(-length);}
-			var minutes = Math.floor(timeInSeconds / 60);
-			var seconds = timeInSeconds - minutes * 60;
-			return str_pad_left(minutes,'0',2)+':'+str_pad_left(seconds,'0',2);
-		}
-	}
+  watch: {
+    TimerEnabled(value) {
+      if (value) {
+        setTimeout(() => {
+          this.timeRemaining--;
+        }, 1000);
+      }
+    },
+    timeRemaining: {
+      handler(value) {
+        if (value > 0 && this.TimerEnabled) {
+          setTimeout(() => {
+            this.timeRemaining--;
+          }, 1000);
+        }
+      },
+      immediate: true,
+    },
+  },
+  filters: {
+    secondsFormatter(timeInSeconds) {
+      function str_pad_left(string, pad, length) {
+        return (new Array(length + 1).join(pad) + string).slice(-length);
+      }
+      var minutes = Math.floor(timeInSeconds / 60);
+      var seconds = timeInSeconds - minutes * 60;
+      return (
+        str_pad_left(minutes, "0", 2) + ":" + str_pad_left(seconds, "0", 2)
+      );
+    },
+  },
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
