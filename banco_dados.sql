1697beed883145e9089d81bd5810f759c943f1de
Joel Marcos Graf <55204995+Joel-Graf@users.noreply.github.com>
2021-06-09 19:40:57 -0300

    Update banco_dados.sql

--- banco_dados.sql
+++ banco_dados.sql
@@ -1,65 +1,56 @@
--- CREATE DATABASE
-CREATE DATABASE pac3 WITH OWNER = postgres ENCODING = 'UTF8' CONNECTION
-LIMIT = -1;
--- POPULATE DATABASE
-BEGIN;
--- -- CREATE TYPES
-CREATE TYPE status_partida AS ENUM('ANDAMENTO', 'VITORIA', 'DERROTA');
--- -- CREATE TABLES
 CREATE TABLE IF NOT EXISTS professores (
-  id SERIAL PRIMARY KEY,
-  nome VARCHAR(255) UNIQUE NOT NULL,
-  usuario VARCHAR(255) UNIQUE NOT NULL,
-  senha VARCHAR(255) NOT NULL
+  id INTEGER PRIMARY KEY,
+  nome VARCHAR(255) UNIQUE,
+  usuario VARCHAR(255) UNIQUE,
+  senha VARCHAR(255)
 );
 CREATE TABLE IF NOT EXISTS alunos (
-  id SERIAL PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   id_professor INTEGER,
-  nome VARCHAR(255) UNIQUE NOT NULL,
-  total_pontos INTEGER DEFAULT 0 NOT NULL,
+  nome VARCHAR(255) UNIQUE,
+  total_pontos INTEGER DEFAULT 0,
   CONSTRAINT fk_professores_alunos FOREIGN KEY(id_professor) REFERENCES professores(id)
 );
 CREATE TABLE IF NOT EXISTS biomas (
-  id SERIAL PRIMARY KEY,
-  nome varchar(255) UNIQUE NOT NULL
+  id INTEGER PRIMARY KEY,
+  nome varchar(255) UNIQUE
 );
 CREATE TABLE IF NOT EXISTS animais (
-  id SERIAL PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   id_bioma INTEGER,
-  nome VARCHAR(255) UNIQUE NOT NULL,
-  imagem VARCHAR(255) NOT NULL,
-  som VARCHAR(255) NOT NULL,
-  descricao VARCHAR(255) NOT NULL,
-  alimentacao VARCHAR(255) NOT NULL,
+  nome VARCHAR(255) UNIQUE,
+  imagem VARCHAR(255),
+  som VARCHAR(255),
+  descricao VARCHAR(255),
+  alimentacao VARCHAR(255),
   CONSTRAINT fk_biomas_animais FOREIGN KEY(id_bioma) REFERENCES biomas(id)
 );
 CREATE TABLE IF NOT EXISTS zoologicos (
-  id SERIAL PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   id_aluno INTEGER,
   CONSTRAINT fk_alunos_zoologicos FOREIGN KEY(id_aluno) REFERENCES alunos(id)
 );
 CREATE TABLE IF NOT EXISTS zoo_anim (
-  id SERIAL PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   id_zoologico INTEGER,
   id_animal INTEGER,
   CONSTRAINT fk_zoo_anim_zoologicos FOREIGN KEY(id_zoologico) REFERENCES zoologicos(id),
   CONSTRAINT fk_zoo_anim_animais FOREIGN KEY(id_animal) REFERENCES animais(id)
 );
 CREATE TABLE IF NOT EXISTS dificuldades (
-  id SERIAL PRIMARY KEY,
-  numero_cartas INTEGER NOT NULL,
-  tempo_limite INTEGER NOT NULL
+  id INTEGER PRIMARY KEY,
+  numero_cartas INTEGER,
+  tempo_limite INTEGER
 );
 CREATE TABLE IF NOT EXISTS partidas (
-  id SERIAL PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   id_aluno INTEGER,
   id_bioma INTEGER,
   id_dificuldade INTEGER,
-  tempo INTEGER NOT NULL,
-  status status_partida,
-  pontos INTEGER NOT NULL,
+  tempo INTEGER,
+  status VARCHAR(255),
+  pontos INTEGER,
   CONSTRAINT fk_alunos_partidas FOREIGN KEY(id_aluno) REFERENCES alunos(id),
   CONSTRAINT fk_biomas_partidas FOREIGN KEY(id_bioma) REFERENCES biomas(id),
   CONSTRAINT fk_dificuldades_partidas FOREIGN KEY(id_dificuldade) REFERENCES dificuldades(id)
 );
-COMMIT;
