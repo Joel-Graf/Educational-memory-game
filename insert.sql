fbc3a345c093a216aa1132db9dd44ef532fdb096
Joel Marcos Graf <55204995+Joel-Graf@users.noreply.github.com>
2021-06-11 20:30:40 -0300

    Adiciona sql de insert

---
+++ insert.sql
@@ -0,0 +1,59 @@
+INSERT INTO professores (nome, usuario, senha) VALUES
+  ('professor1', 'professor1', 'senha1'),
+  ('professor2', 'professor2', 'senha2'),
+  ('professor3', 'professor3', 'senha3'),
+  ('professor4', 'professor4', 'senha4'),
+  ('professor5', 'professor5', 'senha5'),
+  ('professor6', 'professor6', 'senha6');
+
+INSERT INTO alunos (id_professor, nome, total_pontos) VALUES
+  (1, 'aluno1', 1),
+  (1, 'aluno2', 3),
+  (2, 'aluno3', 4),
+  (2, 'aluno4', 0),
+  (3, 'aluno5', 3),
+  (6, 'aluno6', 0);
+  (5, 'aluno7', 0),
+  (6, 'aluno8', 0),
+  (6, 'aluno9', 10);
+
+INSERT INTO biomas (nome) VALUES
+  ('Campo'),
+  ('Deserto'),
+  ('Floresta'),
+  ('Oceano');
+
+INSERT INTO animais (id_bioma, nome, imagem) VALUES
+  (4, 'Água-viva', 'agua_viva'),
+  (4, 'Arraia', 'arraia'),
+  (4, 'Bagre', 'bagre'),
+  (4, 'Baleia Azul', 'baleia_azul'),
+  (4, 'Baleia Orca', 'baleia_orca'),
+  (4, 'Camarão', 'camarao'),
+  (4, 'Cavalo-marinho', 'cavalo_marinho'),
+  (4, 'Estrela-do-mar', 'estrela_do_mar'),
+  (4, 'Foca', 'foca'),
+  (4, 'Golfinho', 'golfinho'),
+  (4, 'Peixe-palhaço', 'peixe_palhaço'),
+  (4, 'Polvo', 'polvo'),
+  (4, 'Tartaruga', 'tartaruga'),
+  (4, 'Tubarão', 'tubarao');
+
+INSERT INTO dificuldades (numero_cartas, tempo_limite) VALUES
+  (8, 120),
+  (14, 240),
+  (20, 300);
+
+INSERT INTO partidas (id_aluno, id_bioma, id_dificuldade, status) VALUES
+  (1, 1, 3, 'FINALIZADO'),
+  (1, 2, 1, 'FINALIZADO'),
+  (2, 1, 3, 'FINALIZADO'),
+  (5, 4, 1, 'FINALIZADO'),
+  (3, 4, 2, 'FINALIZADO'),
+  (4, 4, 1, 'FINALIZADO'),
+  (3, 4, 2, 'FINALIZADO'),
+  (4, 4, 3, 'FINALIZADO'),
+  (1, 4, 3, 'FINALIZADO'),
+  (3, 2, 1, 'FINALIZADO'),
+  (4, 3, 2, 'FINALIZADO'),
+  (5, 1, 2, 'FINALIZADO');
