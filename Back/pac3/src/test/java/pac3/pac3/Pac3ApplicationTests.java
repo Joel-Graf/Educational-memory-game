5f2e6ac04e7ed7dbdb8409b562ff99db2ec78679
Joel Marcos Graf <55204995+Joel-Graf@users.noreply.github.com>
2021-06-12 18:26:33 -0300

    Criado projeto do backend

---
+++ Back/pac3/src/test/java/pac3/pac3/Pac3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pac3.pac3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pac3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
