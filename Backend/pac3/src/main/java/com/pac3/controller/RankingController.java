83a162e5211e86300b7ffb8d3e75a4f05e18fa7d
wechistian <84050979+wechistian@users.noreply.github.com>
2021-06-22 22:33:13 -0300

    RankingController

---
+++ Backend/pac3/src/main/java/com/pac3/controller/RankingController.java
@@ -0,0 +1,27 @@
+package com.pac3.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pac3.model.Aluno;
+import com.pac3.repository.AlunoRepository;
+
+@RestController
+@RequestMapping("/pac3")
+public class RankingController {
+
+	private AlunoRepository alunoRepository;
+
+	RankingController(AlunoRepository alunoRepository) {
+		this.alunoRepository = alunoRepository;
+	}
+
+	@GetMapping("/ranking")
+	List<Aluno> obterAlunos() {
+		return alunoRepository.findAll();
+	}
+	
+}
--- Backend/pac3/src/main/java/com/pac3/model/Professor.java
+++ Backend/pac3/src/main/java/com/pac3/model/Professor.java
@@ -32,5 +32,47 @@ public class Professor {
 		this.usuario = usuario;
 		this.senha = senha;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Set<Aluno> getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(Set<Aluno> alunos) {
+		this.alunos = alunos;
+	}
+	
+	
 	
 }
