c1c6a0a6e8e39c4721939bf82d02abbff556f825
Joel Marcos Graf <55204995+Joel-Graf@users.noreply.github.com>
2021-07-07 19:57:38 -0300

    Consertando leveis bloqueados

--- Front/src/components/AppLevelSelectCard.vue
+++ Front/src/components/AppLevelSelectCard.vue
@@ -1,40 +1,35 @@
 <template id="interface">
   <div>
-    <router-link
-      to="/Game"
-      style="cursor:auto; text-decoration:none"
+    <v-card
+      elevation="5"
+      class="mx-auto my-6"
+      width="324"
+      height="332"
+      v-on="!Level.isLevelLocked ? { click: () => criaJogo() } : {}"
+      :disabled="Level.isLevelLocked"
     >
-      <v-card
-        elevation="5"
-        class="mx-auto my-6"
-        width="324"
-        height="332"
-        @click="criaJogo"
-        :disabled="Level.isLevelLocked"
-      >
-        <v-img height="260" class="mx-3 mb-2">
-          <v-card-title
-            class="
-              d-flex
-              justify-space-between
-              text-uppercase
-              font-weight-medium
-            "
-          >
-            <span>{{ Dificuldade.nome }}</span>
-            <span>{{ Level.nome }}</span>
-          </v-card-title>
+      <v-img height="260" class="mx-3 mb-2">
+        <v-card-title
+          class="
+            d-flex
+            justify-space-between
+            text-uppercase
+            font-weight-medium
+          "
+        >
+          <span>{{ Dificuldade.nome }}</span>
+          <span>{{ Level.nome }}</span>
+        </v-card-title>
 
-          <div
-            v-if="Level.isLevelLocked"
-            class="d-flex justify-center align-center"
-            style="height: 100%"
-          >
-            <v-icon x-large style="transform: scale(3)">mdi-lock-alert</v-icon>
-          </div>
-        </v-img>
-      </v-card>
-    </router-link>
+        <div
+          v-if="Level.isLevelLocked"
+          class="d-flex justify-center align-center"
+          style="height: 100%"
+        >
+          <v-icon x-large style="transform: scale(3)">mdi-lock-alert</v-icon>
+        </div>
+      </v-img>
+    </v-card>
   </div>
 </template>
 
@@ -57,6 +52,7 @@ export default {
   methods: {
     criaJogo() {
       this.$store.commit('criaJogo', { level: this.Level, dificuldade: this.Dificuldade})
+      this.$router.push('Game')
     }
   },
 };
